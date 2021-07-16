2b65ab5954c40e1ad51de68c90c32d6a94ff35ac
Matti <mattigilmore89@gmail.com>
2021-07-16 00:40:41 -0400

    fixed  errors &channges y to "y"

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,23 +3,23 @@ run_loop=True
 while  run_loop==True:
     #1==rock 2==paper 3==scissors
     computer=random.randint(1,3)
-    print("pc pick:"+str(computer))
+    #print("pc pick:"+str(computer))
     pick_loop=True
     replay_loop=True
-    y = "y"  # forgot to make all the y's strings i'll fix it later
-    replay=y
+    
+    replay="y"
 
     while pick_loop==True:
         print("pick 1)rock 2)paper 3)scissors \n")
         player=input()
-        print(type(player))
+        #print(t"y"pe(pla"y"er))
         if player=="1" or player=="2" or player=="3":
             player=int(player)
             if player==computer:
                 print("It  is a tie would you like to play again y/n \n")
                 replay=input()
                 while replay_loop==True:
-                        if replay==y:
+                        if replay=="y":
                            pick_loop=False
                            break
                         else:
@@ -27,62 +27,63 @@ while  run_loop==True:
                             break
             elif player==1:
                     if computer==2:
-                        print("You LOSE;/( would you like to try again y/n \n")
+                        print("you LOSE;(  would you like to try again y/n \n")
                         while replay_loop==True:
                            replay=input()
-                           if replay==y:
+                           if replay=="y":
                              pick_loop=False
                              break
                            else:
                              run_loop=pick_loop=False
                              break
                     else:
-                         print("You WIN!!!! would you like to try again y/n \n")
+                         print("you WIN!!!! would you like to try again y/n \n")
                          while replay_loop==True:
-                            repley=input()
-                            if replay==y:
+                            replay=input()
+                            if replay=="y":
                               pick_loop=False
                               break
                             else:
                               run_loop=pick_loop=False
                               break
-        elif player==2:
-             if computer==3:
-                 print("You LOSE;/( would you like to try again y/n \n")
-                 while replay_loop==True:
-                        repley=input()
-                        if replay==y:
+            elif player==2:
+                 if computer==3:
+                     print("you LOSE;(  would you like to try again y/n \n")
+                     while replay_loop==True:
+                        replay=input()
+                    
+                        if replay=="y":
                             pick_loop=False
                             break
                         else:
                             run_loop=pick_loop=False
                             break
                  else:
-                     print("You WIN!!!! would you like to try again y/n \n")
+                     print("you WIN!!!! would you like to try again y/n \n")
                      while replay_loop==True:
-                         repley=input()
-                         if replay==y:
+                         replay=input()
+                         if replay=="y":
                             pick_loop=False
                             break
                          else:
                             run_loop=pick_loop=False
                             break
-        elif player==3:
+        else:
             if computer==1:
-                print("You LOSE;/( would you like to try again y/n \n")
+                print("you LOSE;(  would you like to try again y/n \n")
                 while replay_loop==True:
-                    repley=input()
-                    if replay==y:
+                    replay=input()
+                    if replay=="y":
                         pick_loop=False
                         break
                     else:
                         run_loop=pick_loop=False
                         break
             else:
-                print("You WIN!!!! would you like to try again y/n \n")
+                print("you WIN!!!! would you like to try again y/n \n")
                 while replay_loop==True:
-                    repley=input()
-                    if replay!=y:
+                    replay=input()
+                    if replay!="y":
                         pick_loop=False
                         break
                     else:
@@ -92,7 +93,7 @@ while  run_loop==True:
 
 
 
-        else:
+    else:
             print("please select valid choice \n")
 
 print("thank you for playing")
